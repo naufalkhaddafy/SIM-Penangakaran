97ea4a96b531dece865aa167aecd387e42167f16
naufalkhaddafy <naufalkhaddafy@webmail.umm.ac.id>
2021-12-09 22:11:48 +0700

    SEMANGATTTTT

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,28 +2,40 @@
 @section('title', 'Dashboard')
 
 @section('content')
-<body>
-    <div class="box box-primary">
 
-        <div style="padding: 20px; text-align: center">
-            <h1>Selamat Datang di Aplikasi Absensi</h1>
+    <body>
+        <div class="box box-primary">
+            <div style="padding: 20px; text-align: center">
+                <h1>Selamat Datang di Aplikasi Absensi</h1>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3>150</h3>
+                            <p>New Orders</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-bag"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box bg-success">
+                    <div class="inner">
+                        <h3>53<sup style="font-size: 20px">%</sup></h3>
 
-        </div>
-        <div class="row">
-    <div class="col-lg-3 col-6">
-    <!-- small box -->
-    <div class="small-box bg-info">
-        <div class="inner">
-        <h3>150</h3>
-
-        <p>New Orders</p>
-        </div>
-        <div class="icon">
-        <i class="ion ion-bag"></i>
-        </div>
-        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-    </div>
-    </div>
-</div>
-</body>
+                        <p>Bounce Rate</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-stats-bars"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+    </body>
 @endsection
