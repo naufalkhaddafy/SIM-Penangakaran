e3463a0d2ac8496841fbf495dbba5dc17c5f7773
naufalkhaddafy <naufalkhaddafy@webmail.umm.ac.id>
2021-12-09 19:55:06 +0700

    Semangat!!!!!!

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,28 +3,27 @@
 
 @section('content')
 <body>
+    <div class="box box-primary">
 
-        <div class="box box-primary">
+        <div style="padding: 20px; text-align: center">
+            <h1>Selamat Datang di Aplikasi Absensi</h1>
 
-            <div style="padding: 20px; text-align: center">
-                <h1>Selamat Datang di Aplikasi Absensi</h1>
-
-            </div>
-             <div class="row">
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-info">
-              <div class="inner">
-                <h3>150</h3>
-
-                <p>New Orders</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-bag"></i>
-              </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
         </div>
-    </body>
+        <div class="row">
+    <div class="col-lg-3 col-6">
+    <!-- small box -->
+    <div class="small-box bg-info">
+        <div class="inner">
+        <h3>150</h3>
+
+        <p>New Orders</p>
+        </div>
+        <div class="icon">
+        <i class="ion ion-bag"></i>
+        </div>
+        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+    </div>
+    </div>
+</div>
+</body>
 @endsection
