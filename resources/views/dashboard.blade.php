da6482876aaae4dc930dc468effd83cf93790495
naufalkhaddafy <naufalkhaddafy@webmail.umm.ac.id>
2021-12-09 19:15:33 +0700

    Semangat

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,15 @@
+@extends('template.template')
+@section('title', 'Dashboard')
+
+@section('content')
+<body>
+
+        <div class="box box-primary">
+
+            <div style="padding: 20px; text-align: center">
+                <h1>Selamat Datang di Aplikasi Absensi</h1>
+
+            </div>
+        </div>
+    </body>
+@endsection
--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -161,22 +161,21 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Dashboard</h1>
+            <h1 class="m-0">@yield('title')</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard v1</li>
+              <li class="breadcrumb-item active">@yield('title')</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
       </div><!-- /.container-fluid -->
     </div>
     <!-- /.content-header -->
-
     <!-- Main content -->
     <section class="content">
-
+        @yield('content')
     </section>
     <!-- /.content -->
   </div>
