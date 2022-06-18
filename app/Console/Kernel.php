d3b891d92a244c07672bb837541e1df0214784b8
naufalkhaddafy <naufalkhaddafy@webmail.umm.ac.id>
2022-06-18 14:37:43 +0700

    Repair Template

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,38 +19,38 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->call(function () {
-            //get relation produksis table
-            $allProduksi = Produksi::all()->load('kandang', 'jadwal');
-            //get all produksi not null by kandang data
-            $NotNullProduksi = [];
-            foreach ($allProduksi as $produksi) {
-                if ($produksi->kandang !== null) {
-                    $NotNullProduksi[] = $produksi;
-                }
-            }
-            // collect data last produksi based on kandang
-            $LastProduksiByKandang = collect($NotNullProduksi)->groupBy('kandang_id')->map(function ($item) {
-                return $item->last();
-            });
-            //get value jadwal from
-            $JadwalProduksi = $LastProduksiByKandang->map(function ($item) {
-                return $item->jadwal;
-            });
-            //Update kategori kandang berdasarkan tanggal
-            $UpdateKandang = $JadwalProduksi->map(function ($item) {
-                if ($item->tgl_akan_bertelur_end < date('Y-m-d')) {
-                    //get id kandang from $item
-                    $item->produksi->kandang->kategori = 'Ganti Bulu';
-                    $item->produksi->kandang->save();
-                    // return 'Success Get Function';
-                } else {
-                    // must give a notifikasi
-                    // return 'Tidak Update';
-                }
-                return $item;
-            });
-        })->daily();
+        // $schedule->call(function () {
+        //     //get relation produksis table
+        //     $allProduksi = Produksi::all()->load('kandang', 'jadwal');
+        //     //get all produksi not null by kandang data
+        //     $NotNullProduksi = [];
+        //     foreach ($allProduksi as $produksi) {
+        //         if ($produksi->kandang !== null) {
+        //             $NotNullProduksi[] = $produksi;
+        //         }
+        //     }
+        //     // collect data last produksi based on kandang
+        //     $LastProduksiByKandang = collect($NotNullProduksi)->groupBy('kandang_id')->map(function ($item) {
+        //         return $item->last();
+        //     });
+        //     //get value jadwal from
+        //     $JadwalProduksi = $LastProduksiByKandang->map(function ($item) {
+        //         return $item->jadwal;
+        //     });
+        //     //Update kategori kandang berdasarkan tanggal
+        //     $UpdateKandang = $JadwalProduksi->map(function ($item) {
+        //         if ($item->tgl_akan_bertelur_end < date('Y-m-d')) {
+        //             //get id kandang from $item
+        //             $item->produksi->kandang->kategori = 'Ganti Bulu';
+        //             $item->produksi->kandang->save();
+        //             // return 'Success Get Function';
+        //         } else {
+        //             // must give a notifikasi
+        //             // return 'Tidak Update';
+        //         }
+        //         return $item;
+        //     });
+        // })->daily();
     }
 
     /**
