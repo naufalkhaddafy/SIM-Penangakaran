1be31422f440fd884e90d5e266d93a34368577ca
naufalkhaddafy <naufalkhaddafy@webmail.umm.ac.id>
2022-06-22 06:52:01 +0700

    Repair Schedulling

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -11,6 +11,15 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     /**
+     * Get the timezone that should be used by default for scheduled events.
+     *
+     * @return \DateTimeZone|string|null
+     */
+    protected function scheduleTimezone()
+    {
+        return ('Asia/Jakarta');
+    }
+    /**
      * Define the application's command schedule.
      *
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
