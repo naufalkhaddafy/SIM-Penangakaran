5ecb2af4c89979131d83ca2a5a4a0035f62cb85c
naufalkhaddafy <naufalkhaddafy@webmail.umm.ac.id>
2022-08-02 22:14:24 +0700

    set redirect to dashboard

--- resources/views/admin-lte/template.blade.php
+++ resources/views/admin-lte/template.blade.php
@@ -46,7 +46,7 @@
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="#" class="brand-link">
+            <a href="{{ route('dashboard') }}" class="brand-link">
                 <img src="{{ asset('logo/logo.png') }}" alt="SGT Logo" class="brand-image img-circle elevation-3"
                     style="opacity: .8">
                 <span class="brand-text font-weight-light">SGT Bird Farm</span>
@@ -61,7 +61,7 @@
                             alt="User Image">
                     </div> --}}
                     <div class="info">
-                        <a href="#" class="d-block">{{ Auth::user()->nama_lengkap }}</a>
+                        <a href="{{ route('dashboard') }}" class="d-block">{{ Auth::user()->nama_lengkap }}</a>
                     </div>
                 </div>
 
